0e7f1f3cb4d6924b10eeae6497f372de391336a8
UncleSamChowder <745603@student.dusd.net>
2024-12-13 13:50:00 -0800

    I added script

--- P4FinalCepero/Assets/Scripts/GameController.cs
+++ P4FinalCepero/Assets/Scripts/GameController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
     // Start is called before the first frame update
     void Awake ()
     {
@@ -23,7 +26,10 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     public void BirdDied()
